c0c1f1b49badf16d3753c31bb84bbd8c31ecabd7
Phil Stainer <philstainer.dev@gmail.com>
2020-05-09 20:56:35 +0100

    Update changePassword test to use bcrypt

--- backend/src/__tests__/user.changePassword.test.js
+++ backend/src/__tests__/user.changePassword.test.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs'
+
 import {dbConnect, dbDisconnect} from '#root/utils/dbConnection'
 import graphqlCall from '#root/utils/graphqlCall'
 import {user} from '#root/models'
@@ -25,14 +27,16 @@ afterEach(() => user.deleteMany({}))
 const fakeUser = {
   name: 'Test',
   email: 'test@test.com',
-  password: '$2a$10$hw4J/8YsYiUCu4jEhWpxBeZP0eV4bB91svZXHh.spK7BmuPs9gRJy',
+  password: 'Password123!',
 }
+
+const newPassword = 'NewPaSSword123!'
 
 test('returns error not logged in', async () => {
   const variables = {
-    currentPassword: fakeUser.password,
-    password: 'changedPass123!!',
-    confirmPassword: 'changedPass123!!',
+    currentPassword: '',
+    password: '',
+    confirmPassword: '',
   }
 
   const {errors} = await graphqlCall(CHANGE_PASSWORD_MUTATION, null, variables)
@@ -41,7 +45,7 @@ test('returns error not logged in', async () => {
 })
 
 test('returns error when empty strings', async () => {
-  const emptyVariables = {
+  const variables = {
     currentPassword: '',
     password: '',
     confirmPassword: '',
@@ -56,7 +60,7 @@ test('returns error when empty strings', async () => {
   const {errors} = await graphqlCall(
     CHANGE_PASSWORD_MUTATION,
     context,
-    emptyVariables,
+    variables,
   )
 
   expect(errors[0].message).toMatch(/currentPassword/i)
@@ -65,8 +69,8 @@ test('returns error when empty strings', async () => {
 
 test('returns error when password and confirmPassword do not match', async () => {
   const variables = {
-    currentPassword: 'password',
-    password: 'Password1234@',
+    currentPassword: fakeUser.password,
+    password: newPassword,
     confirmPassword: 'I do not match',
   }
 
@@ -87,9 +91,9 @@ test('returns error when password and confirmPassword do not match', async () =>
 
 test('returns error when user not found', async () => {
   const variables = {
-    currentPassword: 'password',
-    password: 'Password1234@',
-    confirmPassword: 'Password1234@',
+    currentPassword: fakeUser.password,
+    password: newPassword,
+    confirmPassword: newPassword,
   }
 
   const context = {
@@ -111,9 +115,9 @@ test('returns error when passwords do not match', async () => {
   const createdUser = await user.create(fakeUser)
 
   const variables = {
-    currentPassword: 'Password1234@@',
-    password: 'ChangedPassword123@',
-    confirmPassword: 'ChangedPassword123@',
+    currentPassword: 'I dont match',
+    password: newPassword,
+    confirmPassword: newPassword,
   }
 
   const context = {
@@ -132,12 +136,14 @@ test('returns error when passwords do not match', async () => {
 })
 
 test('returns status when password has been updated', async () => {
-  const createdUser = await user.create(fakeUser)
+  const hashed = await bcrypt.hash(fakeUser.password, 1)
+
+  const createdUser = await user.create({...fakeUser, password: hashed})
 
   const variables = {
-    currentPassword: 'Password1234@',
-    password: 'ChangedPassword123@',
-    confirmPassword: 'ChangedPassword123@',
+    currentPassword: fakeUser.password,
+    password: newPassword,
+    confirmPassword: newPassword,
   }
 
   const context = {
@@ -147,11 +153,12 @@ test('returns status when password has been updated', async () => {
   }
 
   const {data} = await graphqlCall(CHANGE_PASSWORD_MUTATION, context, variables)
+
   const updatedUser = await user
     .findById(createdUser._id)
     .select('password')
     .lean()
 
-  expect(updatedUser.password).not.toEqual(fakeUser.password)
+  expect(updatedUser.password).not.toEqual(hashed)
   expect(data.changePassword).toHaveProperty('status', 'Success')
 })
