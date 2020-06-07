dfee532f6a2e77cf686bcb88e9781bf3cbf09041
Phil Stainer <philstainer.dev@gmail.com>
2020-06-07 13:17:49 +0100

    Add dockerignore and tweek jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['node_modules/.*'],
   collectCoverage: true,
   testRegex: '(/__tests__/.*-test|(\\.|/)(test|spec))\\.jsx?$',
   testEnvironment: 'node',
